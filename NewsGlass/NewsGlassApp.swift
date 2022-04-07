d107670abbd3d3ff5310759ea0ff2c3aa58aa90b
Andrew <andrewmagdyanis@gmail.com>
2022-04-07 15:44:03 +0200

    Initial Commit

---
+++ NewsGlass/NewsGlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsGlassApp.swift
+//  NewsGlass
+//
+//  Created by Andrew Magdy on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsGlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
